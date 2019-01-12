985e70658c93be7b49d86e1632d3be2abf22a792
HonzaZimaas <ja.podzimek@gmail.com>
2019-01-12 16:17:31 +0100

    next commit

--- web_scraping/web_scraping/spiders/web_scrapper.py
+++ web_scraping/web_scraping/spiders/web_scrapper.py
@@ -1,17 +1,15 @@
 import scrapy
 
 
-class WebScrapper(scrapy.Spider):
-    name = "WebScrapper"
+class Example01(scrapy.Spider):
+    name = "Example01"
 
     start_urls = [
         'http://scrapy.org'
     ]
 
     def parse(self, response):
-        filename = 'WebScrapper.html'
+        filename = 'Example01_Scraping.html'
 
         with open(filename, 'wb') as f:
             f.write(response.body)
-
-
